e6521b30b2cdcdd12a5f41efd5b77ef22692e4c8
Deekshitha <deekshithas37@gmail.com>
2023-11-13 19:00:45 +0530

    host update api added

--- carrentalsystem/src/main/java/com/carrentalsystem/main/controller/HostController.java
+++ carrentalsystem/src/main/java/com/carrentalsystem/main/controller/HostController.java
@@ -1,12 +1,17 @@
 package com.carrentalsystem.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.carrentalsystem.main.dto.HostDto;
+import com.carrentalsystem.main.exception.InvalidIdException;
 import com.carrentalsystem.main.model.Host;
 import com.carrentalsystem.main.model.User;
 import com.carrentalsystem.main.service.HostService;
@@ -33,4 +38,20 @@ public class HostController {
 		host = hostService.postHost(host);		
 		return host;
 }
+	@PutMapping("/update/{hid}")
+	public ResponseEntity<?> updateHost(@PathVariable("hid") int hid,@RequestBody HostDto hostDto){
+		try {
+			Host host = hostService.getById(hid);
+			if(hostDto.getHostEmail()!=null)
+				host.setHostEmail(hostDto.getHostEmail());
+			if(hostDto.getHostName()!=null)
+				host.setHostName(hostDto.getHostName());
+			if(hostDto.getHostContact()!=null)
+				host.setHostContact(hostDto.getHostContact());
+			host= hostService.postHost(host);
+			return ResponseEntity.ok().body(host);	
+		}catch(InvalidIdException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
 	}
--- carrentalsystem/src/main/java/com/carrentalsystem/main/dto/HostDto.java
+++ carrentalsystem/src/main/java/com/carrentalsystem/main/dto/HostDto.java
@@ -1,49 +1,31 @@
-package com.carrentalsystem.main.model;
+package com.carrentalsystem.main.dto;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 
-@Entity
-public class Host {
+public class HostDto {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private int hostId; 
-	private String hostEmail;	
+	private String hostEmail;
+	private String hostPass;
 	private String hostName;
 	private String hostContact;
-	@OneToOne
-	private User user; 
-	@Override
-	public String toString() {
-		return "Host [hostId=" + hostId + ", hostEmail=" + hostEmail + ", hostName="
-				+ hostName + ", hostContact=" + hostContact + ", user=" + user + "]";
-	}
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public int getHostId() {
-		return hostId;
-	}
-	public void setHostId(int hostId) {
-		this.hostId = hostId;
-	}
 	public String getHostEmail() {
 		return hostEmail;
 	}
 	public void setHostEmail(String hostEmail) {
 		this.hostEmail = hostEmail;
 	}
-
+	public String getHostPass() {
+		return hostPass;
+	}
+	public void setHostPass(String hostPass) {
+		this.hostPass = hostPass;
+	}
 	public String getHostName() {
 		return hostName;
 	}
-	
 	public void setHostName(String hostName) {
 		this.hostName = hostName;
 	}
